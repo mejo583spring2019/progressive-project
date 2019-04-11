52e109b1ed0bb7da62c3161214f21b22b37ecace
JP Rickabaugh <jprickabaugh@gmail.com>
2019-04-10 21:46:53 -0400

    vids complete

---
+++ hospital-data/src/components/WakeMedTable/index.js
@@ -0,0 +1,14 @@
+import Table from "../Table";
+import wakemed_drg from "../../data/wakemed/drg"
+
+class WakeMedTable extends Table {
+  tableData = wakemed_drg;
+  columns = [
+      {title: "Code", field: "drg_code"},
+      {title: "Description", field: "drg_description"},
+      {title: "Average Price", field: "avg_price"},
+  ];
+  tableHeader = "WakeMed DRG"
+}
+
+export default WakeMedTable;
