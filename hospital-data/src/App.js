7fa5d3a9b02df5b38c22743d8f5d94a8c1cdb96a
Austin Phillips <cochapel@live.unc.edu>
2019-04-11 17:23:09 -0400

    Final Commit

--- hospital-data/src/App.js
+++ hospital-data/src/App.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
-import Table from "./components/Table";
+import DukeTable from "./components/DukeTable";
+import UNCTable from "./components/UNCTable";
+import WakeMedTable from "./components/WakeMedTable";
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World!
-        <Table />
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/data/duke">Duke DRG</Link>
+              </li>
+              <li>
+                <Link to="/data/unc">UNC DRG</Link>
+              </li>
+              <li>
+                <Link to="/data/wakemed">WakeMed DRG</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Index} />
+          <Route path="/data/duke" component={DukeTable} />
+          <Route path="/data/unc" component={UNCTable} />
+          <Route path="/data/wakemed" component={WakeMedTable} />
+        </div>
+      </Router>
     )
   }
 }
