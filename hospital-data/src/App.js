e201ac53e4ec9040eb3b3b9c3a0d39146ec5812a
Brooke Fisher <brookenfish1@gmail.com>
2019-04-14 16:42:50 -0400

    Build out the react-router for the different tables

--- hospital-data/src/App.js
+++ hospital-data/src/App.js
@@ -1,26 +1,51 @@
 import React, { Component } from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 import DukeTable from "./components/DukeTable";
 import UNCTable from "./components/UNCTable";
 import WakeTable from "./components/WakeTable";
 
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+/** Hello there */
 class App extends Component {
   render() {
     return (
-      <div>
+      <Router>
         <div>
-          <h1>This is the Duke Table</h1>
-          <DukeTable />
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/data/duke/">Duke DRG</Link>
+              </li>
+              <li>
+                <Link to="/data/unc">UNC DRG</Link>
+              </li>
+              <li>
+                <Link to="/data/wakemed">WakeMed DRG</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Index} />
+          <Route path="/data/duke" component={DukeTable} />
+          <Route path="/data/unc" component={UNCTable} />
+          <Route path="/data/wakemed" component={WakeTable} />
         </div>
-        <div>
-          <h1>This is the UNC Table</h1>
-          <UNCTable />
-        </div>
-        <div>
-          <h1>This is the WakeMed Table</h1>
-          <WakeTable />
-        </div>
-      </div>
+      </Router>
     );
   }
 }
