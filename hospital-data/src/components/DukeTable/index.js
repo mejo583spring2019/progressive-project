4653da1896587803c9c28385413046d4babb7509
“SivonneWard” <sivonne@live.unc.edu>
2019-04-14 01:31:05 -0400

    20 min through the fourth video

---
+++ hospital-data/src/components/DukeTable/index.js
@@ -0,0 +1,13 @@
+import Table from "../Table";
+import dukeDrg from "../../data/duke/drg";// not named index.js, so must call the name of the file
+
+/** Sets up DukeTable data */
+class DukeTable extends Table {
+  tableData = dukeDrg;
+  columns = [
+    { title: "Code", field: "drg_code" },
+    { title: "Description", field: "drg_description" },
+    { title: "Price", field: "avg_price" },
+  ];
+}
+export default DukeTable;
