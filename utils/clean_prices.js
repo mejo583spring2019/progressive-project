c3734433b48e40ada28e7b50089037b632646974
“SivonneWard” <sivonne@live.unc.edu>
2019-04-13 20:56:07 -0400

    finished programmatically cleaning data, cleaned up $ signs and commas throughout

---
+++ utils/clean_prices.js
@@ -0,0 +1,29 @@
+
+const fs = require("fs");
+
+//this replaces dollar signs and commas in prices
+function main() {
+    if (process.argv.length < 3) {
+        process.stderr.write("Please provide the path to the file to be cleaned. \n");
+    }
+    let filepath = process.argv[2];
+    let rawJSON = fs.readFileSync(filepath);
+    let data = JSON.parse(rawJSON);
+    data.forEach(r => {
+        Object.keys(r).forEach(k => {
+            if (k.indexOf("price") >= 0 || k.indexOf("responsibility") >= 0) {
+                let val = r[k];
+                console.log(val);
+                val = val.replace(',', '').replace('$', '');
+                r[k] = val;
+            }
+        });
+    });
+    let finalJSON = JSON.stringify(data);
+    fs.writeFileSync(filepath, finalJSON);
+
+
+
+
+}
+main();
