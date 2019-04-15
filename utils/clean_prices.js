c59cab172a408e34e712ae127fc21927b3c9277d
tianasama <tianaattride@gmail.coM>
2019-04-15 08:55:34 -0400

    Third commit end of video 3

--- utils/clean_prices.js
+++ utils/clean_prices.js
@@ -2,6 +2,26 @@ const fs = require("fs");
 
 /** Replaces dollar signs and commas in prices */
 function main(){
+    // console.log(process.argv);
+    if (process.argv.length < 3){
+        process.stderr.write("Please provide the path to file to clean.\n")
+    }
+    let filepath = process.argv[2];
+    let rawJSON = fs.readFileSync(filepath);
+
+    let data = JSON.parse(rawJSON);
+    data.forEach((r) => {
+        Object.keys(r).forEach(k => {
+            if(k.indexOf("price") >= 0 || k.indexOf("responsibility")>= 0){
+                let val = r[k];
+                val = val.replace(',', '').replace('$','')
+                r[k] = val;
+            }
+        });
+    });
+    let finalJSON = JSON.stringify(data);
+    fs.writeFileSync(filepath, finalJSON);
+
 
 }
 
