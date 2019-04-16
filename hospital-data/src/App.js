69230db61dbf8d4372a11ced52605ce216db0b06
JP Rickabaugh <jprickabaugh@gmail.com>
2019-04-16 19:54:52 -0400

    on vid 12

--- hospital-data/src/App.js
+++ hospital-data/src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable require-jsdoc */ // REMOVE THESE LINES WHEN DONE EDITING
+
 import React, { Component } from "react";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 
@@ -5,9 +7,14 @@ import DukeTable from "./components/DukeTable";
 import UNCTable from "./components/UNCTable";
 import WakeMedTable from "./components/WakeMedTable";
 import BubbleChart from "./components/BubbleChart";
+import GroupChart from "./components/GroupChart";
 
 function Index() {
-  return <h2>Home</h2>;
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
 }
 
 
@@ -25,6 +32,9 @@ class App extends Component {
                 <Link to="/chart">BubbleChart</Link>
               </li>
               <li>
+                <Link to="/chart/group">Group Chart</Link>
+              </li>
+              <li>
                 <Link to="/data/duke">Duke DRG</Link>
               </li>
               <li>
@@ -37,10 +47,11 @@ class App extends Component {
           </nav>
 
           <Route path="/" exact component={Index} />
-          <Route path="/chart" component={BubbleChart} />
-          <Route path="/data/unc" component={UNCTable} />
-          <Route path="/data/duke" component={DukeTable} />
-          <Route path="/data/wakemed" component={WakeMedTable} />
+          <Route path="/chart" exact component={BubbleChart} />
+          <Route path="/chart/group" exact component={GroupChart} />
+          <Route path="/data/unc" exact component={UNCTable} />
+          <Route path="/data/duke" exact component={DukeTable} />
+          <Route path="/data/wakemed" exact component={WakeMedTable} />
         </div>
       </Router>
     );
