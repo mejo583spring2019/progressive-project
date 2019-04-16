d85eba02ed449911b00172e508930496ef6221f9
tianasama <tianaattride@gmail.coM>
2019-04-15 22:21:57 -0400

    Just couldn't remember the last time I committed - finished video 8

--- hospital-data/src/components/BubbleChart/index.js
+++ hospital-data/src/components/BubbleChart/index.js
@@ -7,25 +7,51 @@ import wakemed_drg from "../../data/wakemed/drg"
 
 import "./styles.css";
 
+//COMPONENT CODE BEGINS
 class BubbleChart extends Component {
   el = React.createRef();
   width = 800;
   height = 600;
 
+
+//Constructor
   constructor(props){
     super(props);
     
-    this.dukeData = duke_drg.map(r => {r.name = "duke"; return r});
-    this.uncData = unc_drg.map(r => {r.name = "unc"; return r});
-    this.wakemedData = wakemed_drg.map(r => {r.name = "wakemed"; return r});
+    this.dukeData = duke_drg.map(r => {
+      r.name = "duke"; 
+      r.key= r.name + r.drg_code;
+      return r;
+    });
 
-    this.data = this.dukeData.concat(this.wakemedData, this.uncData);
+    this.uncData = unc_drg.map(r => 
+      {r.name = "unc"; 
+      r.key= r.name + r.drg_code;
+      return r;
+    });
 
+    this.wakemedData = wakemed_drg.map(r => {
+      r.name = "wakemed"; 
+      r.key= r.name + r.drg_code;
+      return r;
+    });
+
+   this.fullData = this.dukeData.concat(this.wakemedData, this.uncData);
+
+
+    this.state = {
+      showDuke: true,
+      showUNC: true,
+      showWakemed: true,
+      data: this.fullData.slice(),
+      selected: null
+    }
   }
 
   
+  //Creates SVG
   createSVG(){
-    return d3
+    this.svg = d3
       .select(this.el)
       .append("svg")
       .attr("width", this.width)
@@ -33,18 +59,35 @@ class BubbleChart extends Component {
       .attr("style", "border: thin red solid");
   }
 
-  drawChart(svg){
-    let data = this.data;
-    data.sort((a, b) => {
-      return parseInt(b.avg_price) - parseInt(a.avg_price);
-    });
+
+  //Draws chart
+  drawChart(){
+    let data = this.state.data;
+  
+    data.sort((a,b) => parseInt(b.avg_price) - parseInt(a.avg_price))
+
     let hierarchalData = this.makeHierarchy(data);
     let packLayout = this.pack([this.width - 5, this.height - 5])
     const root = packLayout(hierarchalData);
  
-    const leaf = svg
+    const groups = this.svg
       .selectAll("g")
-      .data(root.leaves())
+      .data(root.leaves(), (d) => d.data.key);
+
+    const t = d3.transition().duration(800);
+
+    groups
+      .transition(t)
+      .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
+    groups
+      .select("circle")
+      .attr("r", d => d.r);
+    
+      groups
+        .exit()
+        .remove();
+
+    const leaf = groups
       .enter()
       .append("g")
       .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`)
@@ -57,8 +100,8 @@ class BubbleChart extends Component {
         .append("circle")
         .attr("r", d => d.r)
         .attr("fill-opacity", 0.7)
+        .on("click", this.bubbleClicked.bind(this));
       
-    return svg.node();
     }
 
   pack(size) {
@@ -72,15 +115,105 @@ class BubbleChart extends Component {
     .sum(d => d.avg_price);
   }
 
-  componentDidMount() {
-    let svg = this.createSVG();
-    this.drawChart(svg);
+  filterData(newState){
+    newState = {...this.state, ...newState}
+
+    let newData = this.fullData.filter((r) => {
+      return(
+        (r.name === "duke" && newState.showDuke) || 
+        (r.name === "unc" && newState.showUNC) ||
+        (r.name === "wakemed" && newState.showWakemed)
+      );
+    });
+
+    newState.data = newData;
+
+    this.setState(newState);
   }
 
+
+  //Sets button actions, toggles
+  toggleDuke(){
+    this.filterData({showDuke: !this.state.showDuke})
+  }
+
+  toggleUNC(){
+    this.filterData({showUNC: !this.state.showUNC})
+  }
+
+  toggleWakemed(){
+    this.filterData({showWakemed: !this.state.showWakemed})
+  }
+
+  bubbleClicked(bubble){
+    this.setState({ selected: bubble })
+  }
+
+  getTooltip(){
+    let s = this.state.selected;
+    if(s){
+      return (
+        <div className= "tooltip">
+        <p>{s.data.name}</p>
+        <p>{s.data.drg_code}</p>
+        <p>{s.data.avg_price}</p>
+        <p>{s.data.drg_description}</p>
+        <div className="tooltip-tail" />
+        </div>
+        );
+    }
+  }
+
+  componentDidUpdate() {
+    this.drawChart();
+  }
+
+  componentDidMount() {
+    this.createSVG();
+    this.drawChart();
+  }
+
+
+  //Rendering component
   render() {
     return (
+    //Checkboxes
     <div>
       <h2>Bubble Chart</h2>
+      <label htmlFor="duke-cb">
+        <input 
+          id="duke-cb"
+          type="checkbox"
+          checked={this.state.showDuke}
+          onChange= {this.toggleDuke.bind(this)}
+          />
+        Duke
+      </label>
+      <br/>
+      <label htmlFor="unc-cb">
+        <input 
+          id="unc-cb"
+          type="checkbox"
+          checked={this.state.showUNC}
+          onChange= {this.toggleUNC.bind(this)}
+          />
+        UNC
+      </label>
+      <br/>
+      <label htmlFor="wakemed-cb">
+        <input 
+          id="wakemed-cb"
+          type="checkbox"
+          checked={this.state.showWakemed}
+          onChange= {this.toggleWakemed.bind(this)}
+          />
+        WakeMed
+      </label>
+
+      {this.getTooltip()}
+
+
+      {/* Chart */}
       <div id="bubblechart" ref={el => (this.el = el)} />
     </div> 
     );
