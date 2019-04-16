9bef41017cc6a2cfc302cc44d83950a2883f4b40
tianasama <tianaattride@gmail.coM>
2019-04-16 18:43:31 -0400

    Finished video 10

--- hospital-data/src/App.js
+++ hospital-data/src/App.js
@@ -4,18 +4,12 @@ import { HashRouter as Router, Route, Link } from "react-router-dom";
 import DukeTable from './components/DukeTable';
 import UNCTable from './components/UNCTable';
 import WakeMedTable from './components/WakeMedTable';
-import BubbleChart from './components/BubbleChart'
+import BubbleChart from './components/BubbleChart';
+import GroupChart from './components/GroupChart'
+
 
 function Index() {
   return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 
@@ -33,6 +27,9 @@ class App extends Component {
                 <Link to="/chart">Bubble Chart</Link>
               </li>
               <li>
+                <Link to="/chart/group">Group Chart</Link>
+              </li>
+              <li>
                 <Link to="/data/duke">Duke DRG</Link>
               </li>
               <li>
@@ -46,9 +43,10 @@ class App extends Component {
 
           <Route path="/" exact component={Index} />
           <Route path="/chart" exact component={BubbleChart} />
-          <Route path="/data/duke" component={DukeTable} />
-          <Route path="/data/unc" component={UNCTable} />
-          <Route path="/data/wakemed" component={WakeMedTable} />
+          <Route path="/chart/group" exact component={GroupChart} />
+          <Route path="/data/duke" exact component={DukeTable} />
+          <Route path="/data/unc" exact component={UNCTable} />
+          <Route path="/data/wakemed" exact component={WakeMedTable} />
         </div>
       </Router>
     );
