e97ae32d15b4258bd71020c3c3420635da2b5852
“SivonneWard” <sivonne@live.unc.edu>
2019-04-15 22:03:12 -0400

    7 min through 5th video, started editing the router

--- hospital-data/src/App.js
+++ hospital-data/src/App.js
@@ -1,5 +1,26 @@
 import React, { Component } from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import DukeTable from "./components/DukeTable";
+
+/** this is a JSDOC comment */
+/** this is a JSDOC comment.
+ * @return {string}.
+*/
+function Index() {
+  return <h2>Home</h2>;
+}
+/** this is a JSDOC comment.
+ * @return {string}.
+*/
+function About() {
+  return <h2>About</h2>;
+}
+/** this is a JSDOC comment.
+ * @return {string}.
+*/
+function Users() {
+  return <h2>Users</h2>;
+}
 /** this is a JSDOC comment */
 class App extends Component {
   /** this is a JSDOC comment.
@@ -7,11 +28,27 @@ class App extends Component {
    */
   render() {
     return (
-      <div>
-        hello
-        <DukeTable />
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+              <li>
+                <Link to="/users/">Users</Link>
+              </li>
+            </ul>
+          </nav>
 
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+        </div>
+      </Router>
     );
   }
 }
