7b05eb3e47160d99ba54f9cad2a443d0d3474a97
Amy Townsend <amygtownsend@gmail.com>
2019-04-17 12:46:30 -0400

    Finished getting data to display

---
+++ hospital-data/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import DukeTable from "./components/DukeTable";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+        <DukeTable />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ hospital-data/src/components/DukeTable/index.js
@@ -0,0 +1,13 @@
+import Table from "../Table";
+import duke_drg from "../../data/duke/drg";
+
+class DukeTable extends Table {
+  tableData = duke_drg;
+  columns = [
+    { title: "Code", field: "drg_code" },
+    { title: "Description", field: "drg_description" },
+    { title: "Average Price", field: "avg_price" }
+  ];
+}
+
+export default DukeTable;
