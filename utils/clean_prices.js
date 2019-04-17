958dae864c46314761b82a0a0d7d7cd0cb598e2f
Andrea Brucculeri <andrea@brucculeri.com>
2019-04-17 11:30:57 -0400

    linted utils

--- utils/clean_prices.js
+++ utils/clean_prices.js
@@ -1,28 +1,30 @@
 const fs = require("fs");
 
 // replaces dollar signs and commas in prices
+/**
+* main function
+*/
 function main() {
-    console.log(process.argv);
-    if(process.argv.length < 3) {
-        process.stderr.write("please provide the path to the file to clean.\n");
-    }
-    let filepath = process.argv[2];
-    let rawJSON = fs.readFileSync(filepath);
+  if (process.argv.length < 3) {
+    process.stderr.write("please provide the path to the file to clean.\n");
+  }
+  const filepath = process.argv[2];
+  const rawJSON = fs.readFileSync(filepath);
 
-    let data = JSON.parse(rawJSON);
+  const data = JSON.parse(rawJSON);
 
-    data.forEach((r) => {
-        Object.keys(r).forEach(k => {
-            if (k.indexOf("price") >= 0 || k.indexOf("responsibility") >= 0){
-                let val = r[k];
-                val = val.replace(',', '').replace('$', '')
-                r[k] = val;
-            }
-        });
+  data.forEach((r) => {
+    Object.keys(r).forEach((k) => {
+      if (k.indexOf("price") >= 0 || k.indexOf("responsibility") >= 0) {
+        let val = r[k];
+        val = val.replace(",", "").replace("$", "");
+        r[k] = val;
+      }
     });
+  });
 
-    let finalJSON = JSON.stringify(data);
-    fs.writeFileSync(filepath, finalJSON);
+  const finalJSON = JSON.stringify(data);
+  fs.writeFileSync(filepath, finalJSON);
 }
 
 main();
