176a1dbe170160b9b3b4ceb6b5e00e4e434b0110
Taylor Tyson1 <ttay921@live.unc.edu>
2019-04-19 19:59:07 -0400

    Linted Utils

--- utils/clean_duke_drg.js
+++ utils/clean_duke_drg.js
@@ -4,39 +4,38 @@ const fs = require("fs");
 
 const rx = /^(\d+)( - )(.*)/;
 
-/* Clean duke drg data and populate missing fields*/
-
+/** Clean duke drg data and populate missing fields  */
 function main() {
-    let rawJSON = fs.readFileSync("../cleaned_json/duke_drg.csv.json")
-    let data = JSON.parse(rawJSON)
+  const rawJSON = fs.readFileSync("../cleaned_json/duke_drg.csv.json");
+  const data = JSON.parse(rawJSON);
 
-    data.forEach((r) => {
-        let match = rx.exec(r.drg_description);
-        if (match) {
-            r.drg_code = match[1];
-            r.drg_description = match[3];
-        }
-        if (r.insured_avg_patient_responsibility === "-") {
-            r.insured_avg_patient_responsibility = "0";
-        }
-        if (r.uninsured_avg_patient_responsibility === "-") {
-            r.uninsured_avg_patient_responsibility = "0";
-        }
+  data.forEach((r) => {
+    const match = rx.exec(r.drg_description);
+    if (match) {
+      r.drg_code = match[1];
+      r.drg_description = match[3];
+    }
+    if (r.insured_avg_patient_responsibility === "-") {
+      r.insured_avg_patient_responsibility = "0";
+    }
+    if (r.uninsured_avg_patient_responsibility === "-") {
+      r.uninsured_avg_patient_responsibility = "0";
+    }
 
-        delete r.field6;
-        delete r.field7;
-        delete r.field8;
-        delete r.field9;
-        delete r.field10;
+    delete r.field6;
+    delete r.field7;
+    delete r.field8;
+    delete r.field9;
+    delete r.field10;
 
-        Object.keys(r).forEach(k => {
-            let val = r[k];
-            val = val.trim()
-        })
+    Object.keys(r).forEach((k) => {
+      let val = r[k];
+      val = val.trim();
     });
+  });
 
-    let finalJSON = JSON.stringify(data);
-    fs.writeFileSync("../cleaned_json/duke_drg.csv.json", finalJSON);
+  const finalJSON = JSON.stringify(data);
+  fs.writeFileSync("../cleaned_json/duke_drg.csv.json", finalJSON);
 }
 
-main(); 
+main();
