1f299f3ee351325ef7851b814fde8986dbbe743a
Amy Townsend <amygtownsend@gmail.com>
2019-04-19 17:41:56 -0400

    Linted utils

--- utils/clean_duke_drg.js
+++ utils/clean_duke_drg.js
@@ -6,11 +6,11 @@ const rx = /^(\d+)( - )(.*)/;
 
 /** Clean the Duke DRG data and populate missing fields. */
 function main() {
-  let rawJSON = fs.readFileSync("../cleaned_json/duke_drg.csv.json");
-  let data = JSON.parse(rawJSON);
+  const rawJSON = fs.readFileSync("../cleaned_json/duke_drg.csv.json");
+  const data = JSON.parse(rawJSON);
 
-  data.forEach(r => {
-    let match = rx.exec(r.drg_description);
+  data.forEach((r) => {
+    const match = rx.exec(r.drg_description);
     if (match) {
       r.drg_code = match[1];
       r.drg_description = match[3];
@@ -31,10 +31,10 @@ function main() {
       let val = r[k];
       val = val.trim();
       r[k] = val;
-    })
+    });
   });
 
-  let finalJSON = JSON.stringify(data);
+  const finalJSON = JSON.stringify(data);
   fs.writeFileSync("../cleaned_json/duke_drg.csv.json", finalJSON);
 }
 
