0390cf7b5c043fba02d0b3062311cd800a5dfa06
“SivonneWard” <sivonne@live.unc.edu>
2019-04-19 22:51:09 -0400

    Linted utils

--- utils/clean_prices.js
+++ utils/clean_prices.js
@@ -1,29 +1,25 @@
 
 const fs = require("fs");
 
-//this replaces dollar signs and commas in prices
+/** this replaces dollar signs and commas in prices */
 function main() {
-    if (process.argv.length < 3) {
-        process.stderr.write("Please provide the path to the file to be cleaned. \n");
-    }
-    let filepath = process.argv[2];
-    let rawJSON = fs.readFileSync(filepath);
-    let data = JSON.parse(rawJSON);
-    data.forEach(r => {
-        Object.keys(r).forEach(k => {
-            if (k.indexOf("price") >= 0 || k.indexOf("responsibility") >= 0) {
-                let val = r[k];
-                console.log(val);
-                val = val.replace(",", "").replace('$', '');
-                r[k] = val;
-            }
-        });
+  if (process.argv.length < 3) {
+    process.stderr
+        .write("Please provide the path to the file to be cleaned. \n");
+  }
+  const filepath = process.argv[2];
+  const rawJSON = fs.readFileSync(filepath);
+  const data = JSON.parse(rawJSON);
+  data.forEach((r) => {
+    Object.keys(r).forEach((k) => {
+      if (k.indexOf("price") >= 0 || k.indexOf("responsibility") >= 0) {
+        let val = r[k];
+        val = val.replace(",", "").replace("$", "");
+        r[k] = val;
+      }
     });
-    let finalJSON = JSON.stringify(data);
-    fs.writeFileSync(filepath, finalJSON);
-
-
-
-
+  });
+  const finalJSON = JSON.stringify(data);
+  fs.writeFileSync(filepath, finalJSON);
 }
 main();
